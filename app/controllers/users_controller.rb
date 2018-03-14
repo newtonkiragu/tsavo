4b5c76e25bb5cbe62a9cbe6dbe0abc95bb83e1c9
karanunewton <karanunewton4@gmail.com>
2018-03-14 04:53:58 +0300

    updated crud functionality for user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
   def new
+  @user=User.new
+end
+
+def create
+  @user = User.new(params[:user_params])
+  if @user.save
+    redirect_to root_url, :notice =>"Signed Up!"
+  else
+    render "new", :notice =>"Please check that your username and/or email is correct"
   end
+end
+
+private
+def user_params
+  params.require(:user).permit(:first_name, :last_name, :username, :email)
+end
+
 end
