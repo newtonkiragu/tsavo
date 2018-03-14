522e0aa4b979551e117f947eb1c08b6ac9bb65ba
karanunewton <karanunewton4@gmail.com>
2018-03-14 04:52:52 +0300

    created the user model adding validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
 class User < ApplicationRecord
+  # attr_accessible :first_name, :last_name, :username, :email, :phone_number
+
+attr_accessor :email
+
+validates_confirmation_of :email
+validates :email, :on => :create, :presence => true, :uniqueness => true
+validates :username, :presence => true, :uniqueness => true
+
+def self.authenticate(username, email)
+  user=find_by_email(email)
+  if user && user.email == email
+    user
+  else
+    nil
+  end
+end
 end
