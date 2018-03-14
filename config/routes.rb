a73dacda619263da521e48a6e57e9c8d6204ae38
karanunewton <karanunewton4@gmail.com>
2018-03-14 04:59:00 +0300

    updated my routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,13 @@ Rails.application.routes.draw do
 
   get 'users/new'
 
+  get "log_out" => "sessions#destroy", :as => "log_out"
+  get "log_in" => "sessions#new", :as => "log_in"
+  get "sign_up" => "users#new", :as => "sign_up"
+  root :to => "users#new"
+
+  resources :users
+  resources :sessions
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
