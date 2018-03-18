6c44c579ef3547bbd7305db2f2d3c7e6b7f31d29
karanunewton <karanunewton4@gmail.com>
2018-03-18 13:10:33 +0300

    updated everything

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,11 @@ Rails.application.routes.draw do
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
   # get "sign_up" => "users#new", :as => "sign_up"
-  root :to => "users#new"
+  root :to => "pages#landing"
 
   resources :users
   resources :sessions
-  
+
   # the landing page
   match '/home',to: 'pages#home', via: :get
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
